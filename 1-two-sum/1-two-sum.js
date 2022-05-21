2cedcf03dbf4a1ce55df976e42db6ee97d8d2e05
Mahmoud Mohamed <44137098+wezaa@users.noreply.github.com>
2022-05-21 16:39:28 +0200

    Time: 157 ms (28.69%), Space: 42.1 MB (93.49%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  let indexes = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        indexes.push(i);
+        indexes.push(j);
+      }
+    }
+  }
+  return indexes;
+};
+
