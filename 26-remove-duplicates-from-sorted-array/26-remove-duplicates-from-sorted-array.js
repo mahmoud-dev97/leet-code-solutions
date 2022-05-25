7aeac0e78d440594c0f56153a10008f7425190cb
Mahmoud Mohamed <44137098+wezaa@users.noreply.github.com>
2022-05-25 17:30:13 +0200

    Time: 137 ms (32.83%), Space: 44.2 MB (92.10%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+ 
+        if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+            continue;
+        }
+        // We will update the array in place
+        nums[count] = nums[i];
+        count++;
+    }
+    return count;
+};
