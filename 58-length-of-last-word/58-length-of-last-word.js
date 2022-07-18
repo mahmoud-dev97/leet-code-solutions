08ae0fb1cdb1bf37cc760a373c21aff4600e27f3
Mahmoud Mohamed <44137098+wezaa@users.noreply.github.com>
2022-07-18 02:30:50 +0200

    Time: 118 ms (7.36%), Space: 42.2 MB (31.42%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let arr = s.split(" ").filter((e) => e.length > 0);
+  return arr[arr.length - 1].length;
+};
